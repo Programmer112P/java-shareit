d65e4f53b288b8063696a3937e87ef0eb2ff09cb
Kirill <reskovkirill@yandex.ru>
2023-07-10 10:36:46 +0300

    fix pipeline (9)

--- src/main/java/ru/practicum/shareit/shared/errorhandler/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/shared/errorhandler/ErrorHandler.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
