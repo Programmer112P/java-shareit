df20100fecdab72cd51b570932acc6a4b64f3bd9
Kirill <reskovkirill@yandex.ru>
2023-07-27 17:06:39 +0300

    add tests (2)

--- src/main/java/ru/practicum/shareit/shared/errorhandler/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/shared/errorhandler/ErrorHandler.java
@@ -2,10 +2,7 @@ package ru.practicum.shareit.shared.errorhandler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.HttpMessageConversionException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingRequestHeaderException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
